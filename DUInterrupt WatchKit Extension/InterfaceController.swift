52d16e020d5b97cc612ce58dc9a2fa13588e9e43
Aditya Bose <0127bosea@gmail.com>
2019-02-24 15:18:41 -0500

    Initial Commit

---
+++ DUInterrupt WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  DUInterrupt WatchKit Extension
+//
+//  Created by Aditya Bose on 2/24/19.
+//  Copyright © 2019 Aditya Bose. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ DUInterrupt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DUInterrupt
+//
+//  Created by Aditya Bose on 2/24/19.
+//  Copyright © 2019 Aditya Bose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
